7ef834d9cddef70d0b725d27e402a3ec2b5a518f
roofimon <roofimon@gmail.com>
2013-03-16 08:17:37 +0700

    Took out ActionMailer for testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,14 +36,5 @@ RegistrationSystem::Application.configure do
   config.assets.debug = true
 
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :domain               => "gmail.com",
-      :user_name            => "roofimon@gmail.com",
-      :password             => "I,jvogiH;lN0y'",
-      :authentication       => 'plain',
-      :enable_starttls_auto => true  }
+
 end
